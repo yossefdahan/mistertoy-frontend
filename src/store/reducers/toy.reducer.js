cd868c3396bab1ff9a10646509e0741494c77786
yossefdahan <yossefdahan1@gmail.com>
2024-03-26 15:51:12 +0200

    Add CRUD in toy.reducer

--- src/store/reducers/toy.reducer.js
+++ src/store/reducers/toy.reducer.js
@@ -1,7 +1,11 @@
 import { toyService } from "../../services/toy.service.js";
 
 export const SET_TOYS = 'SET_TOYS'
+export const REMOVE_TOY = 'REMOVE_TOY'
+export const ADD_TOY = 'ADD_TOY'
+export const UPDATE_TOY = 'UPDATE_TOY'
 
+export const SET_FILTER_BY = 'SET_FILTER_BY'
 
 const initialState = {
     toys: [],
@@ -11,6 +15,27 @@ export function toyReducer(state = initialState, action = {}) {
     switch (action.type) {
         case SET_TOYS:
             return { ...state, toys: action.toys }
+
+        case REMOVE_TOY:
+            return { ...state, toys: state.toys.filter(toy => toy._id !== action.toyId) }
+
+        case ADD_TOY:
+            return {
+                ...state,
+                toys: [...state.toys, action.toy]
+            }
+
+        case UPDATE_TOY:
+            return {
+                ...state,
+                toys: state.toys.map(toy => toy._id === action.toy_id ? action.toy : toy)
+            }
+
+        case SET_FILTER_BY:
+            return {
+                ...state,
+                filterBy: { ...state.filterBy, ...action.filterBy }
+            }
 
         default:
             return state
